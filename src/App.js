8b29f7efe30876773c5e616cd15888585c20c14c
Iraklis Roussos <iraklisroussos@gmail.com>
2023-05-15 19:15:24 +1000

    uploading project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Row from "./components/Row"
+import requests from './requests';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+    </div>
+  );
+}
+
+export default App;
