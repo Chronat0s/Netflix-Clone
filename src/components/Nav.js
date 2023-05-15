e905df0a19f94bdc90f4758de88bd8d2d99c1136
Iraklis Roussos <iraklisroussos@gmail.com>
2023-05-15 19:33:03 +1000

    changing logos to avoid anti-malware software interpretting website as scam

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -19,8 +19,8 @@ const Nav = () => {
         <div className={`nav ${show && "nav_black"}`}>
             <img 
               className="nav_logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158"
-              alt="Netflix Logo"
+              src="https://www.kadencewp.com/wp-content/uploads/2020/10/alogo-1.png"
+              alt="placeholder Logo"
             />
             <img 
               className="nav_avatar"
